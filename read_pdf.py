55d796bd95039d2e25f273319a615cf369713027
Superrama <superrama86@gmail.com>
2026-09-16 19:55:29 +0100

    reade the pdf file

--- read_pdf.py
+++ read_pdf.py
@@ -1,6 +1,16 @@
 from pathlib import Path
-from pypdf import PdtReader
+from pypdf import PdfReader
 
 pdf_path=Path("cvriri.pdf")
 
-reader = 
+reader = PdfReader(pdf_path)
+
+print(f"Nombre de pages : {len(reader.pages)}")
+
+text = ""
+
+for page in reader.pages:
+    text += page.extract_text() or ""
+
+print("\n--- DÉBUT DU TEXTE ---\n")
+print(text[:3000])
